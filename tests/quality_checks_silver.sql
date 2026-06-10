aaa9b5af178739fa81496dd00015d9ba882c09e4
Bartek <153931393+rikiadonis@users.noreply.github.com>
2026-06-10 11:24:04 +0200

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -13,13 +13,15 @@ Script Purpose:
 
 Usage Notes:
   - Run these checks after data loading Silver Layer.
-  - Investiage and resolve any discrepancies found during the checks.
+  - Investigate and resolve any discrepancies found during the checks.
 ===============================================================================
 */
+
 
 -- ============================================================
 -- Checking 'silver.cmr_cust_info'
 -- ============================================================
+
 -- Check for NULLs or duplicates in Primary Key
 -- Expectation: No Results
 SELECT
@@ -51,9 +53,11 @@ SELECT DISTINCT
   cst_gndr
 FROM silver.cmr_cust_info
 
+
 -- ============================================================
 -- Checking 'silver.cmr_prd_info'
 -- ============================================================
+
 -- Check for NULLs or duplicates in Primary Key
 -- Expectation: No Results
 SELECT
@@ -94,8 +98,50 @@ SELECT
 FROM bronze.cmr_prd_info
 WHERE prd_start_dt > prd_end_dt
 
+-- ============================================================
+-- Checking 'silver.cmr_sales_details'
+-- ============================================================
+
+-- Check for unwanted spaces
+-- Expectation: No Result 
+SELECT
+	sls_ord_num,
+	sls_prd_key
+FROM silver.cmr_sales_details
+WHERE sls_ord_num != TRIM(sls_ord_num)
+OR sls_prd_key != TRIM(sls_prd_key)
+
+-- Check for Invalid Date Orders
+-- Expectation: No Result
+SELECT
+	*
+FROM silver.cmr_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
+
+-- Check Data Consistency between Sales, Quantity, and Price
+-- >> Sales = Quantity * Price
+-- >> Price = Sales / Quantity
+-- >> Value must not be NULL, Zero, or Negative
+-- Expectation: No Result
+SELECT DISTINCT
+	sls_sales,
+	sls_quantity,
+	sls_price
+FROM silver.cmr_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_price != sls_sales / sls_quantity 
+OR sls_price IS NULL OR sls_quantity IS NULL
+OR sls_sales IS NULL
+OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
 
 
+-- ============================================================
+-- Checking 'silver.erp_cust_az12'
+-- ============================================================
 
-
-
+-- Check for unwanted spaces
+SELECT
+	*
+FROM silver.erp_cust_az12
+WHERE cid != TRIM(cid)
+OR gen != TRIM(gen)

f2183c9a045bda30195c23fb92f4371af256f4a9
Bartek <153931393+rikiadonis@users.noreply.github.com>
2026-08-02 06:11:13 +0200

    Update quality_checks_gold.sql

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -1,1 +1,32 @@
+/*
+==========================================================================================================
+Quality Checks
+==========================================================================================================
+Script Purpose:
+    This script performes quality checks to validate the integrity, consistency, 
+    and accuracy of the Gold Layer. These checks ensure:
+    - Uniqueness of surrogate keys in dimension table.
+    - Referential integrity between fact and dimension tabels.
+    - Validation of relationships in the data model for analytical purposes.
 
+Usage Notes:
+    - Run these checks after data loading Silver Layer.
+    - Investigate and resolve any discrepancies found during the checks.
+===========================================================================================================
+
+*/
+
+-- =========================================================================================================
+-- Checking 'gold.dim_customers'
+-- =========================================================================================================
+-- Check for Uniqeness of Customer Key in gold.dim_customer
+-- Expectation: No results
+
+SELECT
+	customer_key,
+  COUNT(*) AS duplicate_count
+FROM gold.fact_sales
+GROUP BY customer_key
+HAVING COUNT(*) > 1
+
+--===========================================================================================================
